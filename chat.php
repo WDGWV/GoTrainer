570f6c032d1e65d3d7c3b68bc517987642376fa7
Wesley de Groot <wes@vista.aero>
2018-08-11 17:07:55 +0200

    Auto-Update

--- chat.php
+++ chat.php
@@ -2,13 +2,13 @@
 
 $chat = array(
         // user    // text          // rank // lat  // lon
-        array("WDGWV", "Hello welcome", "admin", "lat", "lon"),
-        array("Carloooooo", "Hello welcome", "admin", "lat", "lon"),
-        array("WDGWV", "Hey Carloooooo", "admin", "lat", "lon"),
-        array("GUESTY", "Hey Trainers", "guest", "lat", "lon"),
-        array("MODER", "Hey Trainers", "moderator", "lat", "lon"),
-        array("VIP", "Hey Trainers", "vip", "lat", "lon"),
-        array("GUESTY", "Hey Trainers", "guest", "lat", "lon"),
+        'id0' => array("WDGWV", "Hello welcome", "admin", "lat", "lon"),
+        'id1' => array("Carloooooo", "Hello welcome", "admin", "lat", "lon"),
+        'id2' => array("WDGWV", "Hey Carloooooo", "admin", "lat", "lon"),
+        'id3' => array("GUESTY", "Hey Trainers", "guest", "lat", "lon"),
+        'id4' => array("MODER", "Hey Trainers", "moderator", "lat", "lon"),
+        'id5' => array("VIP", "Hey Trainers", "vip", "lat", "lon"),
+        'id6' => array("GUESTY", "Hey Trainers", "guest", "lat", "lon"),
 );
 
 file_put_contents('chat.json', json_encode($chat));
