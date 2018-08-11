dbeadd8dc8deb2e8bd65c32241849493985b3261
Wesley de Groot <wes@vista.aero>
2018-08-11 17:14:56 +0200

    Auto-Update

--- chat.php
+++ chat.php
@@ -2,13 +2,26 @@
 
 $chat = array(
         // user    // text          // rank // lat  // lon
-        'chatMessage' => array("WDGWV", "Hello welcome", "admin", "lat", "lon"),
-        'id1' => array("Carloooooo", "Hello welcome", "admin", "lat", "lon"),
-        'id2' => array("WDGWV", "Hey Carloooooo", "admin", "lat", "lon"),
-        'id3' => array("GUESTY", "Hey Trainers", "guest", "lat", "lon"),
-        'id4' => array("MODER", "Hey Trainers", "moderator", "lat", "lon"),
-        'id5' => array("VIP", "Hey Trainers", "vip", "lat", "lon"),
-        'id6' => array("GUESTY", "Hey Trainers", "guest", "lat", "lon"),
+        'chatMessage0' => array(
+        	'user' => "WDGWV",
+        	'message' => "Hello welcome",
+        	'status' => "admin",
+        	'latitude' => "lat",
+        	'longitude' => "lon"
+        ),
+        'chatMessage1' => array(
+        	'user' => "Carloooooo",
+        	'message' => "Hello welcome",
+        	'status' => "admin",
+        	'latitude' => "lat",
+        	'longitude' => "lon"
+        ),
+        // 'id1' => array("Carloooooo", "Hello welcome", "admin", "lat", "lon"),
+        // 'id2' => array("WDGWV", "Hey Carloooooo", "admin", "lat", "lon"),
+        // 'id3' => array("GUESTY", "Hey Trainers", "guest", "lat", "lon"),
+        // 'id4' => array("MODER", "Hey Trainers", "moderator", "lat", "lon"),
+        // 'id5' => array("VIP", "Hey Trainers", "vip", "lat", "lon"),
+        // 'id6' => array("GUESTY", "Hey Trainers", "guest", "lat", "lon"),
 );
 
 file_put_contents('chat.json', json_encode($chat));
