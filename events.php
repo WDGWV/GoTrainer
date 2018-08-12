2f0aac6bdd34e1c2720bb43067bfa692fa1466f6
Wesley de Groot <wes@vista.aero>
2018-08-12 20:30:41 +0200

    Auto-Update

--- events.php
+++ events.php
@@ -1,9 +1,27 @@
 <?php
 
 $events = array(
-	array("Evee", "blah", "11-08-2018", "11:00", "13:00", "imageURL"),
-    array("Evee", "blah", "12-08-2018", "11:00", "13:00", "imageURL"),
-    array("Niantic did something.", "Description", "12-12-2018", "12:00", "12:00", "imageURL")
+	array(
+		'title' => "Evee Community day 1",
+		'message' => "blah",
+		'begin' => mktime("11-08-2018 11:00"),
+		'end' => mktime("11-08-2018 14:00"),
+		'image' => "imageURL"
+	),
+    array(
+		'title' => "Evee Community day 2",
+		'message' => "blah",
+		'begin' => mktime("12-08-2018 11:00"),
+		'end' => mktime("12-08-2018 14:00"),
+		'image' => "imageURL"
+    ),
+    array(
+		'title' => "Niantic did something",
+		'message' => "blah",
+		'begin' => mktime("12-12-2018 11:00"),
+		'end' => mktime("12-12-2018 14:00"),
+		'image' => "imageURL"
+    )
 );
 
 file_put_contents('events.json', json_encode($events));
