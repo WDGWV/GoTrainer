79bcfaf060cb4db99bed8ecdf47adf1d62ab2fa5
Wesley De Groot <wes@vista.aero>
2018-08-13 12:23:49 +0200

    Update news.php

--- news.php
+++ news.php
@@ -4,22 +4,22 @@ $news = array(
 	array(
 		'title' => "Evee Community day 1",
 		'message' => "blah",
-		'begin' => mktime("11-08-2018 11:00"),
-		'end' => mktime("11-08-2018 14:00"),
+		'begin' => date('c', mktime("11-08-2018 11:00")),
+		'end' => date('c', mktime("11-08-2018 14:00")),
 		'image' => "imageURL"
 	),
     array(
 		'title' => "Evee Community day 2",
 		'message' => "blah",
-		'begin' => mktime("12-08-2018 11:00"),
-		'end' => mktime("12-08-2018 14:00"),
+		'begin' => date('c', mktime("12-08-2018 11:00")),
+		'end' => date('c', mktime("12-08-2018 14:00")),
 		'image' => "imageURL"
     ),
     array(
 		'title' => "Niantic did something",
 		'message' => "blah",
-		'begin' => mktime("12-12-2018 11:00"),
-		'end' => mktime("12-12-2018 14:00"),
+		'begin' => date('c', mktime("12-12-2018 11:00")),
+		'end' => date('c', mktime("12-12-2018 14:00")),
 		'image' => "imageURL"
     )
 );
