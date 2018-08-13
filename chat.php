69609b23425af0cf70715e25e08f3c9986b09301
Wesley de Groot <wes@vista.aero>
2018-08-13 20:33:27 +0200

    Auto-Update

--- chat.php
+++ chat.php
@@ -6,33 +6,55 @@ $chat = array(
         	'user' => "WDGWV",
         	'message' => "Hello welcome",
         	'status' => "admin",
+                'privacy' => 'global',
                 'team' => 'instinct',
-        	'latitude' => 0.0,
-        	'longitude' => 0.0
+                'latitude' => 52.359734,
+                'longitude' => 4.664674
         ),
         array(
         	'user' => "Carloooooo",
         	'message' => "Hello welcome",
         	'status' => "admin",
+                'privacy' => 'global',
                 'team' => 'instinct',
         	'latitude' => 52.359734,
         	'longitude' => 4.664674
         ),
         array(
                 'user' => "MalformedUser",
-                'message' => "Hello welcome",
+                'message' => "valor number one!",
                 'team' => 'valor',
                 'status' => "KIP",
+                'privacy' => 'global',
                 'latitude' => "EW",
                 'longitude' => "WE"
         ),
         array(
                 'user' => "MalformedUser1",
-                'message' => "Hello welcome",
+                'message' => "mystic number one!",
                 'team' => 'mystic',
                 'status' => "KIP",
+                'privacy' => 'global',
                 'latitude' => "EW",
                 'longitude' => "WE"
+        ),
+        array(
+                'user' => "GOD",
+                'message' => "Instinct number one!",
+                'team' => 'instinct',
+                'status' => "guest",
+                'privacy' => 'global',
+                'latitude' => 52.359734,
+                'longitude' => 4.664674
+        ),
+        array(
+                'user' => "WDGWV",
+                'message' => "Alleen lokale mensen kunnen dit zien :D",
+                'status' => "admin",
+                'privacy' => 'global',
+                'team' => 'local',
+                'latitude' => 52.359734,
+                'longitude' => 4.664674
         ),
 );
 
